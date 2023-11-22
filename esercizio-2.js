9b18dfcbc1eaf1776b9a40f1b0858e0161449e3b
helgacozzo <helgacozzo@gmail.com>
2023-11-22 10:50:05 +0100

    feat/esercizio-2

---
+++ esercizio-2.js
@@ -0,0 +1,20 @@
+// const array = ['antonio', 'Armando', 'gianni', 'filippo', 'andrea'];
+
+const concatenateStrings = (array) => {
+
+    const startWithA = array.filter(str => str.includes('a') || str.includes('A'));
+
+    let concatenatedString = '';
+    startWithA.forEach(str => {
+        concatenatedString += str;
+    });
+
+    if (concatenatedString.length > 10) {
+        concatenatedString = concatenatedString.substring(0, 10);
+    }
+
+    return concatenatedString;
+};
+
+// const result = concatenateStrings(array);
+// console.log(result);
