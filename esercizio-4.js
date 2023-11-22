d8a5a711947cbcc44e6baa4c437f2d08cdcbdd55
helgacozzo <helgacozzo@gmail.com>
2023-11-22 12:46:13 +0100

    feat/esercizio-4

---
+++ esercizio-4.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', () => {
+
+    const button = document.getElementById("button");
+    const input = document.getElementById("input");
+    const error = document.getElementById("errorMessage");
+    const ul = document.getElementById("tasks");
+
+    button.addEventListener("click", () => {
+        const inputValue = input.value;
+
+        if (inputValue === '') {
+
+            error.innerHTML = "Per favore, inserisci del testo.";
+
+        } else {
+
+            error.innerHTML = '';
+
+            const li = document.createElement("li");
+            li.innerHTML = inputValue;
+
+            ul.appendChild(li);
+
+            input.value = '';
+            
+        }
+
+    });
+
+});
